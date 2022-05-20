3dfb42173cf9936ce4c7a5343f608c98e16379f9
”argnblck” <argnblck@gmail.com>
2022-05-20 23:59:00 +0300

    update kata

--- Opposites Attract.js
+++ Opposites Attract.js
@@ -3,7 +3,5 @@ function lovefunc(flower1, flower2){
       return true;
     } else if (flower1%2 != 0 && flower2%2 == 0) {
       return true;
-    } else {
-      return false;
-    }
-  }
+    } else return false;  
+}
