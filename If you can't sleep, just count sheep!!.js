fa58ec192cb1224b629365c566f3f0efd9141e60
”argnblck” <argnblck@gmail.com>
2022-07-15 02:04:17 +0300

    little update

--- If you can't sleep, just count sheep!!.js
+++ If you can't sleep, just count sheep!!.js
@@ -1,3 +1,6 @@
+/* Given a non-negative integer, 3 for example, return a string with a murmur: "1 sheep...2 sheep...3 sheep...". Input will always be valid, i.e. no negative integers. */
+
+
 var countSheep = function (num){
     let murmur = "";
     if (num < 1) {
--- Reversed sequence.js
+++ Reversed sequence.js
@@ -1,3 +1,5 @@
+/* Build a function that returns an array of integers from n to 1 where n>0. */
+
 const reverseSeq = n => {
     if (n < 1) {
       return [];
